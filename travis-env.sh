7ac5b547585d8afab7f8dc02ec96290462a236c7
mikanebu <meiran1991@gmail.com>
2017-09-29 00:35:18 +0600

    [fixed][xs]

--- travis-env.sh
+++ travis-env.sh
@@ -1,3 +1,1 @@
-export profile="{ "id":"b6c7c7180404aeb2863e592a9a72deb9", "username":"Mikanebu"}"
-export id=b6c7c7180404aeb2863e592a9a72deb9
-export username=Mikanebu
+export profile="{"id":"b6c7c7180404aeb2863e592a9a72deb9","username":"Mikanebu"}"
